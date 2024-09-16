d0125bf2b97a9911a2532c7bd2b40e536155427e
Navdeep0912 <w0837256@myscc.ca>
2024-09-16 18:51:06 -0400

    Midterm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Navdeep-Kaur-learning-angular';
+  title:string = 'Navdeep Kaur';
+  assignment:number = 1;
 }
