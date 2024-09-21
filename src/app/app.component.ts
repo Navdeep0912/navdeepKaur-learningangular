a5a5dec5ceaab1cdd1cf1aa77d33083ff7d45727
Navdeep0912 <W0837256@myscc.ca>
2024-09-21 12:34:18 -0400

    Assignment done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {world} from "./world";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title= 'World Data';
 
-  worldData1 : world = {id:1, country:"India", city:"New Delhi",language:"Hindi", food:"Jalebi" };
-  worldData2 : world = {id:2, country:"Canada", city:"Toronto",language:"French", food:"Poutine" };
-  worldData3 : world = {id:3, country:"Australia", city:"Sydney",language:"English", food:"Roast lamb" };
-  worldData4 : world = {id:4, country:"Pakistan", city :"Lahore",language:"Urdu", food:"Nihari" };
-  worldData5 : world = {id:5, country:"Dubai", city:"Abu dhabi",language:"Arabic", food:"Khuzi" };
-  worldData6 : world = {id:6, country:"England", city:"London",language:"British English", food:"Pudding" };
+  worldData : world [] = [
+    {id:1, country:"India", city:"New Delhi",language:"Hindi", food:"Jalebi" },
+    {id:2, country:"Canada", city:"Toronto",language:"French", food:"Poutine" },
+    {id:3, country:"Australia", city:"Sydney",language:"English", food:"Roast lamb" },
+    {id:4, country:"Pakistan", city :"Lahore",language:"Urdu", food:"Nihari" },
+    {id:5, country:"Dubai", city:"Abu dhabi",language:"Arabic", food:"Khuzi" },
+    {id:6, country:"England", city:"London",language:"British English", food:"Pudding" },
+];
 }
