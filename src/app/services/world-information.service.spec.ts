08627b25f2369b3e20a4775d0f5dac7ccedc8205
Navdeep0912 <W0837256@myscc.ca>
2024-10-02 12:48:52 -0400

    Final assignment

---
+++ src/app/services/world-information.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { WorldInformationService } from './world-information.service';
+
+describe('WorldInformationService', () => {
+  let service: WorldInformationService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(WorldInformationService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
