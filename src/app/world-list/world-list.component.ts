d76f4ab0d020ac5142e2355060b72f2d90960aa3
Navdeep0912 <W0837256@myscc.ca>
2024-11-06 21:50:56 -0500

    templates modified step 9

--- src/app/world-list/world-list.component.ts
+++ src/app/world-list/world-list.component.ts
@@ -1,8 +1,7 @@
-import { Component } from '@angular/core';
-import {worldList} from "../../Shared/mockContent";
-import {NgForOf, NgIf} from "@angular/common";
-// @ts-ignore
-import {error} from "@angular/compiler-cli/src/transformers/util";
+import { Component, OnInit } from '@angular/core';
+import { NgForOf, NgIf } from "@angular/common";
+import { WorldInformationService } from "../Services/world-information.service";
+import { worldItem } from "../../Model/worldItem";
 
 @Component({
   selector: 'app-world-list',
@@ -12,10 +11,32 @@ import {error} from "@angular/compiler-cli/src/transformers/util";
     NgIf
   ],
   templateUrl: './world-list.component.html',
-  styleUrl: './world-list.component.css'
+  styleUrls: ['./world-list.component.css']
 })
-export class WorldListComponent {
+export class WorldListComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'country', 'city', 'language', 'food'];
+  worldList: worldItem[] = [];
+  error: string | null = null;
 
-    protected readonly worldList = worldList;
-  protected readonly error = error;
+  constructor(private worldService: WorldInformationService) {
+  }
+
+  ngOnInit() {
+    this.worldService.getContent().subscribe({
+      next: (data: worldItem[]) => {
+        this.worldList = data;
+        this.error = null;
+      },
+      error: err => {
+        this.error = 'Error fetching world data';
+        console.error("Error fetching world data", err);
+      },
+      complete: () => console.log("World data fetch complete!")
+    });
+  }
+
+  selectedWorldItem?: worldItem;
+  selectWorldItem(worldItems: worldItem): void {
+    this.selectedWorldItem = worldItems;
+  }
 }
