0d617ab2c1f97cd3e35bd62b1a5b0ffeff793bb5
Navdeep0912 <W0837256@myscc.ca>
2024-11-13 15:08:47 -0500

    pipes created

---
+++ src/app/pipes/world-item-name.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { WorldItemNamePipe } from './world-item-name.pipe';
+
+describe('WorldItemNamePipe', () => {
+  it('create an instance', () => {
+    const pipe = new WorldItemNamePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/world-item-name.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {worldItem} from "../../Shared/Models/worldItem";
+import {worldList} from "../../Shared/mockContent";
+
+@Pipe({
+  name: 'worldItemName',
+  standalone: true
+})
+export class WorldItemNamePipe implements PipeTransform {
+
+  transform(worldItem : worldItem){
+    return `${worldItem.city}, ${worldItem.country}`;
+  }
+}
